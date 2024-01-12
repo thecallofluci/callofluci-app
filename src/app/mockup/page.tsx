16cd84a2de934b9112211d580b57232b43490764
PLAYER 251 <callofluci@pm.me>
2024-01-12 11:33:10 -0600

    mockup page.tsx

--- src/app/mockup/page.tsx
+++ src/app/mockup/page.tsx
@@ -6,6 +6,7 @@ import {
 } from '@/src/app/_components/_next-ui/primitives'
 
 import { Divider } from '@nextui-org/divider'
+import { Link } from '@nextui-org/link'
 
 import wormfood from '@/public/wormfood.jpg'
 import TheMuck from '@/public/TheMuck.jpg'
@@ -13,6 +14,15 @@ import TheMuck from '@/public/TheMuck.jpg'
 import Home1 from '@/public/mockup/Home1.png'
 import Gallery2 from '@/public/mockup/Gallery2.png'
 import AuthenticatedUser3 from '@/public/mockup/AuthenticatedUser3.png'
+import Data from '@/public/mockup/Data.png'
+import End from '@/public/mockup/End.png'
+import Fields from '@/public/mockup/Fields.png'
+import FullWorkflow from '@/public/mockup/FullWorkflow.png'
+import Receipt from '@/public/mockup/Receipt.png'
+import ServerlessFunctions from '@/public/mockup/ServerlessFunctions.png'
+import UserForm from '@/public/mockup/UserForm.png'
+import WorkFlowPart1 from '@/public/mockup/WorkFlowPart1.png'
+import WorkFlowPart2 from '@/public/mockup/WorkFlowPart2.png'
 import callofluci from '@/public/mockup/callofluci.svg'
 
 import LuciImage from '@/src/app/_components/_ui/LuciImage'
@@ -23,12 +33,19 @@ export default function MockupPage() {
 		<>
 			<section className="flex flex-col items-center justify-center gap-3 py-8 md:py-10 w-9/10 mx-auto">
 				<div>
-					<h1 className={lucititle({ color: 'primary' })}>Examples</h1>
+					<h1 className={lucititle({ color: 'primary' })}>Mockups</h1>
 				</div>
 				<div>
 					<p className={lucinote({ color: 'primary', size: 'sm' })}>
-						This page displays custom components used throughout the app.
+						This page displays wireframe mockups of the app from Excalidraw.
 					</p>
+					<Link
+						isExternal
+						href="https://excalidraw.com/#json=44Qao-aY3Kp-nC4MrqJ9p,8-pJN11NaQpxbTwewOPzSg"
+						showAnchorIcon
+					>
+						Excalidraw Wireframes
+					</Link>
 				</div>
 
 				{/* This will force a space equivalent to 3 line breaks */}
@@ -48,8 +65,6 @@ export default function MockupPage() {
 						</h2>
 					</div>
 					<div className="flex flex-wrap justify-center gap-8">
-				
-
 						<div>
 							<h2
 								className={`${lucisubheading({
@@ -66,7 +81,207 @@ export default function MockupPage() {
 									width: 'auto',
 									height: 'auto',
 								}}
-                                className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								3. Observation Page, authenticated users
+							</h2>
+							<LuciImage
+								src={AuthenticatedUser3}
+								alt="AuthenticatedUser3"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								4. WorkFlow Part 1
+							</h2>
+							<LuciImage
+								src={WorkFlowPart1}
+								alt="WorkFlowPart1"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								5. WorkFlow Part 2
+							</h2>
+							<LuciImage
+								src={WorkFlowPart2}
+								alt="WorkFlowPart2"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								6. Fields
+							</h2>
+							<LuciImage
+								src={Fields}
+								alt="Fields"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								7. UserForm
+							</h2>
+							<LuciImage
+								src={UserForm}
+								alt="UserForm"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								8. Receipt
+							</h2>
+							<LuciImage
+								src={Receipt}
+								alt="Receipt"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								8. Submitted Entry
+							</h2>
+							<LuciImage
+								src={End}
+								alt="End"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								9. Anatomy of a Receipt
+							</h2>
+							<LuciImage
+								src={FullWorkflow}
+								alt="FullWorkflow"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								10. Data
+							</h2>
+							<LuciImage
+								src={Data}
+								alt="Data"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+							/>
+						</div>
+
+						<div>
+							<h2
+								className={`${lucisubheading({
+									color: 'secondary',
+								})} text-center pb-1`}
+							>
+								11. Serverless Functions
+							</h2>
+							<LuciImage
+								src={ServerlessFunctions}
+								alt="ServerlessFunctions"
+								sizes="80vw"
+								style={{
+									width: 'auto',
+									height: 'auto',
+								}}
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
 							/>
 						</div>
 
@@ -81,40 +296,14 @@ export default function MockupPage() {
 							<LuciImage
 								src={Gallery2}
 								alt="Gallery2"
-                                sizes="80vw"
+								sizes="80vw"
 								style={{
 									width: 'auto',
 									height: 'auto',
 								}}
-                                className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
+								className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
 							/>
-							
 						</div>
-
-
-						<div>
-							<h2
-								className={`${lucisubheading({
-									color: 'secondary',
-								})} text-center pb-1`}
-							>
-								2. Observation Page, authenticated users
-							</h2>
-							<LuciImage
-								src={AuthenticatedUser3}
-								alt="AuthenticatedUser3"
-                                sizes="80vw"
-								style={{
-									width: 'auto',
-									height: 'auto',
-								}}
-                                className="flex flex-wrap gap-4 items-center overflow-hidden radius-sm rounded-md shadow-medium box-border border-primary-700 border-none"
-							/>
-							
-						</div>
-
-
-						
 					</div>
 				</div>
 			</section>
