9a77ba23fda454e6c57a28346291fc18b015ec85
PLAYER 251 <callofluci@pm.me>
2024-02-06 19:17:17 -0600

    removed primary-600 from LuciImage.tsx to see if it removes blue border

--- src/app/components/ui/LuciImage.tsx
+++ src/app/components/ui/LuciImage.tsx
@@ -9,7 +9,7 @@ interface LuciImageProps extends ImageProps {
 export default function LuciImage({ className, ...props }: LuciImageProps) {
 	return (
 		<div
-			className={`flex flex-wrap gap-4 items-center border-3 border-primary-600 overflow-hidden radius-sm rounded-md border-solid shadow-medium box-border ${className}`}
+			className={`flex flex-wrap gap-4 items-center border-3 border-primary overflow-hidden radius-sm rounded-md border-solid shadow-medium box-border ${className}`}
 		>
 			<div className="overflow-hidden">
 				<NextImage {...props} />
