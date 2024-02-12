1c5be442784480314d7b45d6a3ae5b7043278754
PLAYER 251 <callofluci@pm.me>
2024-02-12 12:27:51 -0600

    testing testing layout

--- src/app/(routes)/(temporary)/testing/layout.tsx
+++ src/app/(routes)/(temporary)/testing/layout.tsx
@@ -7,7 +7,7 @@ import AppWrapper from '@/src/app/components/layout/AppWrapper'
 // Defining the Root Layout component
 export default function TestingLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html suppressHydrationWarning lang="en" className="var(--font-medium)">
+		<html suppressHydrationWarning lang="en" >
 			<head />
 			<body className={clsx('min-h-screen antialiased')}>
 				<Providers themeProps={{ attribute: 'class', defaultTheme: 'luci-light' }}>
